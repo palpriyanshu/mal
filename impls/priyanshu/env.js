40ab79ec28013a4b68ffaa9856d14b61aa319acc
palpriyanshu <pk981878@gmail.com>
2021-08-21 10:49:57 +0530

    add 'def!'  and 'let*' implementation (step 3)

---
+++ impls/priyanshu/env.js
@@ -0,0 +1,35 @@
+const {MalSymbol} = require('./types');
+
+class Env {
+  constructor(outer = null) {
+    this.data = new Map();
+    this.outer = outer;
+  }
+
+  set(key, malValue) {
+    if (!(key instanceof MalSymbol)) {
+      throw `${key} is not a symbol`;
+    }
+    this.data.set(key.symbol, malValue);
+    return malValue;
+  }
+
+  find(key) {
+    if (this.data.has(key.symbol)) {
+      return this;
+    }
+
+    return this.outer && this.outer.find(key);
+  }
+
+  get(key) {
+    const env = this.find(key);
+    if (env === null) {
+      throw `${key.symbol} not found`;
+    }
+
+    return env.data.get(key.symbol);
+  }
+}
+
+module.exports = {Env};
